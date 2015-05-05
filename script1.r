aabeb4ea1e59c4c7486509088f7f61fb5c088755
it757 <it757@yahoo.com>
2015-05-05 16:48:24 -0400

    reading text file from hadoop fs

--- script1.r
+++ script1.r
@@ -6,3 +6,10 @@ nrow(mytxt2)
 mytxt = read.table("/mapr/pmr1/user/pfmapr1/r_test/test_01.txt",header=TRUE)
 system.time(read.table("/mapr/pmr1/user/pfmapr1/r_test/test_01.txt"))
 nrow(mytxt)
+
+
+bash-4.1$ git add .
+bash-4.1$ git commit -m "file saved"
+bash-4.1$ git remote add origin git@github.com:it757/project8.git
+bash-4.1$ git push -u origin master
+bash-4.1$ git status
---
+++ script2.r
@@ -0,0 +1,3 @@
+mytxt2 = read.table("/mapr/pmr1/user/pfmapr1/r_test/retail_prod_FI.txt",sep="|",header=TRUE)
+mytxt2
+nrow(mytxt2)
