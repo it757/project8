283f36bc2c03cf381352120c579434c2c0bdf807
it757 <it757@yahoo.com>
2015-05-06 09:17:11 -0400

    programs committed

--- script2.r
+++ script2.r
@@ -1,3 +1,9 @@
 mytxt2 = read.table("/mapr/pmr1/user/pfmapr1/r_test/retail_prod_FI.txt",sep="|",header=TRUE)
 mytxt2
 nrow(mytxt2)
+
+# Read Text File
+mytxt = read.table("/mapr/pmr1/user/pfmapr1/r_test/test_01.txt",header=TRUE)
+system.time(read.table("/mapr/pmr1/user/pfmapr1/r_test/test_01.txt"))
+nrow(mytxt)
+
